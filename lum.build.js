91f4ee8523dc4aa7eda11886b0e6df62964e6e95
Timothy Totten <2010@totten.ca>
2024-09-13 14:04:49 -0700

    Updated lum.build.js to use the new builder

--- lum.build.js
+++ lum.build.js
@@ -1,40 +1,11 @@
-"use strict";
-
-const D =
+module.exports = function(rb)
 {
-  build: 'docs/build',
-  api: 'docs/api/',
-  changelogs: 'docs/changelogs',
-  ad(from)
-  {
-    const to = D.api+from.replace(/\.md/, '.html');
-    return {from,to}
-  },
-}
-
-const dirs = 
-[
-  D.build,
-  D.api+D.changelogs,
-]
-
-const header = "<html><body>";
-const footer = "</body></html>";
-
-const files = 
-[
-  {
-    from: 'README.md', 
-    to: D.build+'/README.md',
-  },
-  D.ad('docs/TODO.md'),
-  D.ad(D.changelogs+'/index.md'),
-  D.ad(D.changelogs+'/1.0-beta.md'),
-  D.ad(D.changelogs+'/1.x.md'),
-  D.ad(D.changelogs+'/2.x.md'),
-]
-
-module.exports =
-{
-  dirs, files, header, footer,
+  rb.readme
+    .add('docs/TODO.md')
+    .chdir('docs/changelogs')
+    .add('index.md')
+    .add('1.0-beta.md')
+    .add('1.x.md')
+    .add('2.x.md')
+    //.set('debug', true)
 }
