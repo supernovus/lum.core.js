5df397546ad3c2a025c5b16f6a0dbd14e3533a13
Timothy Totten <2010@totten.ca>
2024-09-19 12:38:26 -0700

    fixes and stuff

---
+++ docs/src/index.js
@@ -0,0 +1,142 @@
+/**
+ * Constants and functions for type checks
+ * and other fundamental functions
+ * 
+ * @name module:@lumjs/core.types
+ * @see module:@lumjs/core/types
+ */
+
+/**
+ * Define properties on an object or function
+ * @name module:@lumjs/core.def
+ * @function
+ * @see module:@lumjs/core/types.def
+ */
+
+/**
+ * Define *lazy* properties on an object or function
+ * @name module:@lumjs/core.lazy
+ * @function
+ * @see module:@lumjs/core/types.lazy
+ */
+
+/**
+ * Information about the JS context we're running in
+ * @name module:@lumjs/core.context
+ * @type {module:@lumjs/core/context}
+ */
+
+/**
+ * Persistent state helper
+ * @name module:@lumjs/core.state
+ * @type {module:@lumjs/core/state}
+ */
+
+/**
+ * Get a simplistic debugging stacktrace
+ * @name module:@lumjs/core.stacktrace
+ * @function
+ * @see module:@lumjs/core/meta.stacktrace
+ */
+
+/**
+ * A simple base class for making *abstract* classes
+ * @name module:@lumjs/core.AbstractClass
+ * @see module:@lumjs/core/meta.AbstractClass
+ */
+
+/**
+ * A *factory* for special types of JS `function` constructors
+ * @name module:@lumjs/core.Functions
+ * @see module:@lumjs/core/meta.Functions
+ */
+
+/**
+ * Throw an `Error` saying that a feature is not yet implemented
+ * @name module:@lumjs/core.NYI
+ * @function
+ * @see module:@lumjs/core/meta.NYI
+ */
+
+/**
+ * A function indicating that something is deprecated
+ * @name module:@lumjs/core.deprecated
+ * @function
+ * @see module:@lumjs/core/meta.deprecated
+ */
+
+/**
+ * Assign a getter property to an object that calls the `deprecated`
+ * function with a specific message before returning the proxied value.
+ * @name module:@lumjs/core.wrapDepr
+ * @function
+ * @see module:@lumjs/core/meta.wrapDepre
+ */
+
+/**
+ * Array utility functions «Lazy»
+ * @name module:@lumjs/core.arrays
+ * @type {module:@lumjs/core/arrays}
+ */
+
+/**
+ * Map utility functions «Lazy»
+ * @name module:@lumjs/core.maps
+ * @type {module:@lumjs/core/maps}
+ */
+
+/**
+ * Functions for working with strings and locales «Lazy»
+ * @name module:@lumjs/core.strings
+ * @type {module:@lumjs/core/strings}
+ */
+
+/**
+ * Functions for working with binary flags «Lazy»
+ * @name module:@lumjs/core.flags
+ * @type {module:@lumjs/core/flags}
+ */
+
+/**
+ * Functions for manipulating objects «Lazy»
+ * @name module:@lumjs/core.obj
+ * @type {module:@lumjs/core/obj}
+ */
+
+/**
+ * A wrapper around the Javascript console «Lazy»
+ * @name module:@lumjs/core.console
+ * @type {module:@lumjs/core/console}
+ */
+
+/**
+ * A simplistic implementation of class/object traits «Lazy»
+ * @name module:@lumjs/core.traits
+ * @type {module:@lumjs/core/traits}
+ */
+
+/**
+ * Functions for getting values and properties with fallback defaults «Lazy»
+ * @name module:@lumjs/core.opt
+ * @type {module:@lumjs/core/opt}
+ */
+
+/**
+ * Create a magic *Enum* object «Lazy»
+ * @name module:@lumjs/core.Enum
+ * @function
+ * @see module:@lumjs/core/enum
+ */
+
+/**
+ * Make an object support the *Observable* API «Lazy»
+ * @name module:@lumjs/core.observable
+ * @function
+ * @see module:@lumjs/core/observable
+ */
+
+/**
+ * The Events module «Lazy»
+ * @name module:@lumjs/core.events
+ * @see module:@lumjs/core/events
+ */
